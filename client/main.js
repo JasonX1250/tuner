18010eb605bca32f285353b0db9a5d99c387e2a4
Jason Xiao <JasonX1250@gmail.com>
2020-10-26 18:07:16 -0400

    loads platforms with js instead of static html list of platforms

---
+++ client/main.js
@@ -0,0 +1,21 @@
+const availablePlatforms = ["Spotify", "YouTube"];
+let startPlatform = "";
+
+window.onload = () => {
+    loadMenu();
+};
+
+function loadMenu() {
+    const menu = document.getElementById("menu");
+    for (const platform of availablePlatforms) {
+        const tab = document.createElement("a");
+        tab.href = "startPlatformQuerty.html";
+        tab.onclick = setStartPlatform(platform);
+        tab.appendChild(document.createTextNode(`${platform}`));
+        menu.appendChild(tab);
+    }
+}
+
+function setStartPlatform(platform) {
+    startPlatform = platform;
+}
