98879a2f61ff6897f43a4d437067ed28a3068460
Jason Xiao <JasonX1250@gmail.com>
2020-10-27 05:57:23 -0400

    added html, css, and js to page for searching up playlists to add media to

---
+++ client/SpecifyPlaylistsToAddTo/specifyPlaylistsToAddTo.js
@@ -0,0 +1,37 @@
+window.onload = async () => {
+    await loadMenu();
+    await loadPlatforms();
+    addEndPlatformLogo();
+
+    // add event listener to 'Continue' button
+    document.getElementById("continue-btn").addEventListener("click", specifyPlaylists);
+}
+
+function addEndPlatformLogo() {
+    const endPlatformLogo = document.getElementById("end-platform-logo");
+    const logo = document.createElement("img");
+    logo.id="end-platform-icon";
+    logo.src = `../Logos/${startPlatform}.jpg`;
+    endPlatformLogo.appendChild(logo);
+    const text = document.createElement("div");
+    text.id = "end-platform-name";
+    text.appendChild(document.createTextNode(`${startPlatform}`));
+    endPlatformLogo.appendChild(text);
+}
+
+function specifyPlaylists() {
+    let specification = "find";
+    let query = { "specification": specification};
+    if (document.getElementById("link-existing-playlists").checked) {
+        specification = "link";
+        query["link"] = document.getElementById("existing-playlist-link").value;
+    } else if (document.getElementById("add-new-playlist").checked) {
+        specification = "new";
+        query["title"] = document.getElementById("new-playlist-title").value;
+        if (document.getElementById("privacy-public").checked) {
+            query["privacy"] = "public";
+        } else {
+            query["privacy"] = "private";
+        }
+    }
+}
