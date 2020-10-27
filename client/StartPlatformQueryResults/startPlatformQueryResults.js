5a088d9b5501c03b75fbf752e3a7f504d19ff03c
Jason Xiao <JasonX1250@gmail.com>
2020-10-26 21:13:22 -0400

    reorganized startPlatformQueryResults to a new folder and updated file references accordingly

--- client/StartPlatformQueryResults/startPlatformQueryResults.js
+++ client/StartPlatformQueryResults/startPlatformQueryResults.js
@@ -13,7 +13,7 @@ function addStartPlatformLogo() {
     console.log(startPlatformLogo);
     const logo = document.createElement("img");
     logo.id="start-platform-icon";
-    logo.src = `Logos/${startPlatform}.jpg`;
+    logo.src = `../Logos/${startPlatform}.jpg`;
     startPlatformLogo.appendChild(logo);
     const text = document.createElement("div");
     text.id = "start-platform-name";
