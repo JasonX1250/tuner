39de2f6ef9eabb4ea1ee59bb3c5700c5760bd1eb
alleopp <alapin@umass.edu>
2020-11-09 23:50:53 -0500

    implemented google auth

--- client/Login/login.js
+++ client/Login/login.js
@@ -4,6 +4,7 @@ window.onload = async () => {
 
     // add event listener to 'Search' button
     document.getElementById("login-btn").addEventListener("click", login);
+    document.getElementById("login-youtube").addEventListener("click", authenticate().then(loadClient).then(execute));
 }
 
 async function login() {
@@ -25,3 +26,37 @@ async function login() {
         window.location.href = `${url}`;
     }
 }
+
+
+async function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyBgOYic4sHxOHNDAn_aA0Fmwbc48SwPfO4");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.channels.list({
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "id": [
+        "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "1055829242664-uqpf238cu07143osochbpfirnpceku27.apps.googleusercontent.com"});
+  });
+
