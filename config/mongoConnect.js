b53db86661a107e77b8611be5466a8aca5ca8757
alleopp <alapin@umass.edu>
2020-11-15 17:11:29 -0500

    set up mongoose connection to db

--- config/mongoConnect.js
+++ config/mongoConnect.js
@@ -1,14 +1,17 @@
-var mongodb = require('mongodb');
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://<test>:<wrdad434>@cluster0.yagjq.mongodb.net/<UserDB>?retryWrites=true&w=majority";
-const InitiateMongoServer = async () => {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true , useUnifiedTopology : true
-    }).then(
-    () => {console.log("bla")},
-    err => {console.log(err)});
-    console.log("Connected to DB !!");
-  } 
+const mongoose = require("mongoose");
+const url = "mongodb+srv://Alex:wrdad434@cluster0.yagjq.mongodb.net/UserDB?retryWrites=true&w=majority";
+// const client = new MongoClient(url);
+async function run() {
+    try {
+        await mongoose.connect(url, {useNewUrlParser: true,  useUnifiedTopology: true });
+        console.log("Connected correctly to server");
 
-module.exports = InitiateMongoServer;
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await mongoose.connection.close();
+    }
+}
 
+run().catch(console.dir);
