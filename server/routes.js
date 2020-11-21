256708baed32536070b7c29c7c474a886bda4c04
Jason Xiao <JasonX1250@gmail.com>
2020-11-21 02:11:29 -0500

    implemented endpoint for converting media

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,8 @@ const passport = require('passport');
 const fetch = require('node-fetch');
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-const SPOTIFY_API_KEY = process.env.SPOTIFY_API_KEY;
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const YOUTUBE = "YouTube";
 const SPOTIFY = "Spotify";
 
@@ -15,10 +16,14 @@ const queryYoutubeQuery = async (q, type) => {
     if (response.ok) {
         const data = await response.json();
         for (media of data.items) {
+            let link = `https://youtube.com/watch?v=${media.id.videoId}`;
+            if (type === "playlist") {
+                link = `https://youtube.com/playlist?list=${media.id.playlistId}`;
+            }
             results.push({
                 title: media.snippet.title,
                 author: media.snippet.channelTitle,
-                link: `https://youtube.com/watch?v=${media.id.videoId}`,
+                link: link,
                 img: media.snippet.thumbnails.default.url
             });
         }
@@ -57,22 +62,29 @@ const queryYoutubeLink = async (link, type) => {
 };
 
 const querySpotifyQuery = async (q, type) => {
+    const access_token = await getSpotifyAccessToken();
     const results = [];
     const url = `https://api.spotify.com/v1/search?q=${q}&type=${type}&limit=10`;
     const response = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${SPOTIFY_API_KEY}`
+            "Authorization": `Bearer ${access_token}`
         }
     });
     if (response.ok) {
         const data = await response.json();
         for (media of data[`${type}s`].items) {
+            let image = "";
+            if (type === "album") {
+                image = media.images[0].url;
+            } else {
+                image = media.album.images[0].url;
+            }
             results.push({
                 title: media.name,
                 author: media.artists[0].name,
                 link: media.external_urls.spotify,
-                img: media.album.images[0].url
+                img: image
             });
         }
     }
@@ -80,6 +92,7 @@ const querySpotifyQuery = async (q, type) => {
 }
 
 const querySpotifyLink = async (link, type) => {
+    const access_token = await getSpotifyAccessToken();
     const results = [];
     let mediaId = "";
     switch (type) {
@@ -97,19 +110,92 @@ const querySpotifyLink = async (link, type) => {
     const response = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${SPOTIFY_API_KEY}`
+            "Authorization": `Bearer ${access_token}`
         }
     });
     if (response.ok) {
         const data = await response.json();
+        let image = "";
+        if (type === "album") {
+            image = data.images[0].url;
+        } else {
+            image = data.album.images[0].url;
+        }
         results.push({
             title: data.name,
             author: data.artists[0].name,
             link: data.external_urls.spotify,
-            img: data.album.images[0].url
+            img: image
         });
     }
     return results;
+};
+
+const getSpotifyAccessToken = async () => {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString("base64")}`
+        },
+        body: "grant_type=client_credentials"
+    });
+    if (response.ok) {
+        const access_token = await response.json();
+        return access_token["access_token"];
+    }
+}
+
+const extractMediaFromCollection = async (platform, type, collection) => {
+    const media = [];
+    if (platform === YOUTUBE) {
+        if (type === "Playlist") {
+            let data = {};
+            do {
+                let query = `&playlistId=${collection.link.substring(collection.link.indexOf("list=") + 5)}`;
+                if ("nextPageToken" in data) {
+                    query += `&pageToken=${data.nextPageToken}`;
+                }
+                let url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50" + query + `&key=${GOOGLE_API_KEY}`;
+                let response = await fetch(url);
+                if (response.ok) {
+                    data = await response.json();
+                    for (video of data.items) {
+                        media.push({
+                            title: video.snippet.title,
+                            author: video.snippet.channelTitle,
+                            id: video.snippet.resourceId.videoId,
+                        });
+                    }
+                }
+            } while ("nextPageToken" in data);
+        }
+    } else if (platform === SPOTIFY) {
+        const access_token = await getSpotifyAccessToken();
+        let url = "";
+        if (type === "Playlist") {
+            url = `https://api.spotify.com/v1/playlists/${collection.link.substring(collection.link.indexOf("playlist/") + 9)}/tracks`;
+        } else if (type === "Album") {
+            url = `https://api.spotify.com/v1/playlists/${collection.link.substring(collection.link.indexOf("album/") + 6)}/tracks`;
+        }
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${access_token}`
+            }
+        });
+        if (response.ok) {
+            const data = await response.json();
+            for (item of media.items) {
+                media.push({
+                    title: item.track.name,
+                    author: item.track.artists[0].name,
+                    id: item.track.id,
+                });
+            }
+        }
+    }
+    return media;
 };
 
 router.get("/queryMedia", async (req, res) => {
@@ -143,11 +229,30 @@ router.get("/getSavedPlaylists", (req, res) => {
     res.send(ex2);
 });
 
-router.post("/convertMedia", (req, res) => {
-    const platform = req.body.platform;
-    const mediaToConvert = req.body.media;
-    console.log(req.body);
-    res.send(ex1);
+router.post("/convertMedia", async (req, res) => {
+    let mediaToConvert = [];
+    const results = [];
+    if (req.body.type === "Playlist" || req.body.type === "Album") {
+        mediaToConvert = await extractMediaFromCollection(req.body.startPlatform, req.body.type, req.body.media);
+    } else {
+        mediaToConvert.push(req.body.media);
+    }
+    if (req.body.endPlatform === YOUTUBE) {
+        for (media of mediaToConvert) {
+            const converted = await queryYoutubeQuery(media.title.replace(/[^\w\s]/gi, ""), "video");
+            if (converted.length !== 0) {
+                results.push(converted[0]);
+            }
+        }
+    } else if (req.body.endPlatform === SPOTIFY) {
+        for (media of mediaToConvert) {
+            const converted = await querySpotifyQuery(media.title.replace(/[^\w\s]/gi, ""), "track");
+            if (converted.length !== 0) {
+                results.push(converted[0]);
+            }
+        }
+    }
+    res.send(results);
 });
 
 router.post("/newPlaylist", (req, res) => {
